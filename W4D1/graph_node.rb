d57be840fa0aef0a3a5e50c8b8678bc24ea3c6a3
Daniel Gorter <danieljgorter@gmail.com>
2020-04-04 22:29:28 -0400

    Create GraphNode class and bfs method

---
+++ W4D1/graph_node.rb
@@ -0,0 +1,30 @@
+class GraphNode
+    attr_reader :val, :neighbors
+    attr_writer :neighbors
+
+    def initialize(val)
+        @val = val
+        @neighbors = []
+    end
+
+    def inspect
+        {"value" => val, "neighbors" => neighbors.map(&:val)}.inspect
+    end
+end
+
+def bfs(start_node, target_val)
+    visited = Set.new
+    queue = [start_node]
+
+    until queue.empty?
+        cur_node = queue.shift
+        return cur_node if cur_node.val == target_val
+        visited.add(cur_node)
+
+        cur_node.neighbors.each do |neighbor_node|
+            queue << neighbor_node unless visited.include?(neighbor_node)
+        end
+    end
+
+    nil
+end
