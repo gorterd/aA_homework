68cbde2e4f660e47b436e5603c37bdc60f9cc7ce
Daniel Gorter <danieljgorter@gmail.com>
2020-04-14 21:03:40 -0400

    Extend functionality of ORM

--- W5D3/plays_playwrights_orm/plays.rb
+++ W5D3/plays_playwrights_orm/plays.rb
@@ -18,6 +18,29 @@ class Play
     data = PlayDBConnection.instance.execute("SELECT * FROM plays")
     data.map { |datum| Play.new(datum) }
   end
+  
+  def self.find_by_title(title)
+    datum = PlayDBConnection.instance.execute(<<-SQL, title)
+    SELECT * FROM plays WHERE title = ?
+    SQL
+    
+    Play.new(datum.first)
+  end
+  
+  def self.find_by_playwright(name)
+    data = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT 
+        * 
+      FROM 
+        plays 
+      JOIN 
+        playwrights ON playwright_id = playwrights.id
+      WHERE 
+        playwrights.name = ?
+    SQL
+    
+    data.map { |datum| Play.new(datum) }
+  end
 
   def initialize(options)
     @id = options['id']
@@ -49,3 +72,55 @@ class Play
     SQL
   end
 end
+
+
+class Playwright
+  attr_accessor :id, :name, :birth_year
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
+  end
+  
+  def self.find_by_name(name)
+    datum = PlayDBConnection.instance.execute(<<-SQL, name)
+    SELECT * FROM playwrights WHERE name = ?
+    SQL
+
+    Playwright.new(datum.first)
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    Play.find_by_playwright(self.name)
+  end
+end
+
