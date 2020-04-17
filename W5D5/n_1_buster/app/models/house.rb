ca1e0a3be010fdc1a9126820f23606a321c9f486
Daniel Gorter <danieljgorter@gmail.com>
2020-04-17 02:05:05 -0400

    Implement better queries in n plus 1 project

--- W5D5/n_1_buster/app/models/house.rb
+++ W5D5/n_1_buster/app/models/house.rb
@@ -17,8 +17,16 @@ class House < ApplicationRecord
 
     seeds
   end
-
+  
   def better_seeds_query
     # TODO: your code here
+    plants = self.plants.includes(:seeds)
+    seeds = []
+    plants.each do |plant|
+      seeds << plant.seeds
+    end
+  
+    seeds
+
   end
 end
